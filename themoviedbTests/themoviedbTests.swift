1144c20c6fdbdaeebb2123b7946a364c0115b0a9
danielroble <daniel12dar@gmail.com>
2019-03-05 21:40:23 -0300

    Initial commit

---
+++ themoviedbTests/themoviedbTests.swift
@@ -0,0 +1,34 @@
+//
+//  themoviedbTests.swift
+//  themoviedbTests
+//
+//  Created by Daniel Roble on 3/5/19.
+//  Copyright © 2019 Daniel Roble. All rights reserved.
+//
+
+import XCTest
+@testable import themoviedb
+
+class themoviedbTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
